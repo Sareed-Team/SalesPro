2e399cc02ff386e22460d907c254e529e52fa489
Saeed <dotnettashkent@gmail.com>
2023-03-04 21:29:17 +0500

    Add Entities "Sell" and Enums"Payment,User role,Why get money"

---
+++ PayOffice.Domain/Enums/PaymentType.cs
@@ -0,0 +1,9 @@
+﻿namespace PayOffice.Domain.Enums
+{
+    public enum PaymentType : byte
+    {
+        Cash = 10,
+        PlasticCard = 20,
+        Online = 30
+    }
+}
---
+++ PayOffice.Domain/Enums/UserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace PayOffice.Domain.Enums
+{
+    public enum UserRole : byte
+    {
+        Admin = 10,
+        SuperAdmin = 20,
+        Seller = 30
+    }
+}
