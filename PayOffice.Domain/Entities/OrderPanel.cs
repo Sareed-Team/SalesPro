bfbe06efff50da7a5bf54654220ab2b1568c638b
Saeed <dotnettashkent@gmail.com>
2023-03-04 22:18:37 +0500

    Added other classes

---
+++ PayOffice.Domain/Entities/OrderPanel.cs
@@ -0,0 +1,17 @@
+﻿
+using PayOffice.Domain.Commons;
+using PayOffice.Domain.Enums;
+
+namespace PayOffice.Domain.Entities
+{
+    public class OrderPanel : Auditable
+    {
+        public string ProductName { get; set; }
+        public string FirmName { get; set; }
+        public string Description { get; set; }
+        public int Count { get; set; }
+        public DateTime ArriveTime { get; set; }
+        public decimal TotalAmount { get; set; }
+        public PaymentType Payment { get; set; }
+    }
+}
