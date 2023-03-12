beedc6621799259f815591519a461e053e730af3
Saeed <dotnettashkent@gmail.com>
2023-03-12 21:05:28 +0500

    Added main components

---
+++ PayOffice.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using PayOffice.Domain.Commons;
+using PayOffice.Domain.Enums;
+
+namespace PayOffice.Domain.Entities
+{
+    public class User : Auditable
+    {
+        public string Phone { get; set; }
+        public string UserName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public UserRole userRole { get; set; }
+    }
+}
