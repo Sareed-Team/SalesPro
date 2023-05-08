dfff771d3f906ef88338a3a8b45031c2961879e2
Saeed <dotnettashkent@gmail.com>
2023-05-08 22:21:50 +0500

    Added All Entities

---
+++ PayOffice.Domain/Entities/Commerces/Commerce.cs
@@ -0,0 +1,23 @@
+﻿using PayOffice.Domain.Commons;
+using PayOffice.Domain.Entities.Products;
+using PayOffice.Domain.Entities.Users;
+using PayOffice.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayOffice.Domain.Entities.Commerces
+{
+    public class Commerce : Auditable
+    {
+        public decimal Amount { get; set; }
+        public PaymentType PaymentType { get; set; }
+
+        public long UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ PayOffice.Domain/Entities/Orders/Order.cs
@@ -0,0 +1,22 @@
+﻿using PayOffice.Domain.Commons;
+using PayOffice.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayOffice.Domain.Entities.Orders
+{
+    public class Order : Auditable
+    {
+        public string CompanyName { get; set; }
+        public string AgentName { get; set; }
+        public string AgentPhone { get; set; }
+        public string OrderNames { get; set; }
+        public decimal Price { get; set; }
+        public OrderPaymentType PaymentType { get; set; }
+        public DateTime InComeDay { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
